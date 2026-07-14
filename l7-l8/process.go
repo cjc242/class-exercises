711a511b975972b5348e2558e7b8fa9db76364d7
Christian Johnson <christianjohnson@crc-dot1x-nat-10-239-244-33.bu.edu>
2026-07-14 13:37:56 -0400

    pasted teh starter code given for process.go into the newly created process.go file

---
+++ l7-l8/process.go
@@ -0,0 +1,27 @@
+package main
+
+type Process struct {
+	clock []int
+	pid   int
+	N     int
+}
+
+func (p *Process) Start(N int, PID int) {
+
+}
+
+func (p *Process) Internal() {
+
+}
+
+func (p *Process) Send() []int {
+
+}
+
+func (p *Process) Receive(ts []int) {
+
+}
+
+func Compare(ts1 []int, ts2 []int) int {
+
+}
